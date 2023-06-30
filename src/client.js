6201e2e0fda36837177dcb6e3fc8d05e42560488
Hermerson de Araújo <ne.hermerson@gmail.com>
2023-06-29 23:47:12 -0300

    create async functions

--- src/client.js
+++ src/client.js
@@ -8,6 +8,17 @@ const notesObject = grpc.loadPackageDefinition(notesDefinition);
 
 const noteServiceStub = new notesObject.NoteService('localhost:50051', grpc.credentials.createInsecure());
 
+const listAsync = promisify(noteServiceStub.list.bind(noteServiceStub));
+const findAsync = promisify(noteServiceStub.find.bind(noteServiceStub));
+
+listAsync({})
+  .then((response) => console.log('listAsync', response))
+  .catch((error) => console.error('listAsync', error));
+
+findAsync({ id: 1 })
+  .then((response) => console.log('findAsync', response))
+  .catch((error) => console.error('findAsync', error));
+
 noteServiceStub.list({}, (error, response) => {
   if (error) throw error;
   console.log(response);
