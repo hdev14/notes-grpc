ae67815bc1fdd0c789553fe3638af375ea10fc38
Hermerson de Araújo <ne.hermerson@gmail.com>
2023-07-11 22:07:47 -0300

    add certificates

--- ts/src/client.ts
+++ ts/src/client.ts
@@ -1,8 +1,22 @@
-import { ChannelCredentials } from "@grpc/grpc-js";
+import { Metadata, credentials } from "@grpc/grpc-js";
+import { readFileSync } from "fs";
+import { resolve } from 'path';
 import { NotesClient } from "../proto/notes_grpc_pb";
 import { NoteFindRequest, Void } from "../proto/notes_pb";
 
-const notesClient = new NotesClient('0.0.0.0:50052', ChannelCredentials.createInsecure());
+const rootCert = readFileSync(resolve(__dirname, '../../ca-cert.pem'));
+
+const channelCreds = credentials.createSsl(rootCert);
+// const metaCallback = (_: any, callback: Function) => {
+//   const meta = new Metadata();
+//   meta.add('custom-header', 'test');
+//   callback(null, meta);
+// }
+
+// const callCreds = credentials.createFromMetadataGenerator(metaCallback);
+// const combCreds = credentials.combineChannelCredentials(channelCreds, callCreds);
+
+const notesClient = new NotesClient('0.0.0.0:50052', channelCreds);
 
 notesClient.list(new Void(), (err, response) => {
   if (err) {
@@ -11,7 +25,10 @@ notesClient.list(new Void(), (err, response) => {
   console.log(response.toObject());
 });
 
-notesClient.find(new NoteFindRequest().setId(2), (err, response) => {
+const metadata = new Metadata();
+metadata.add('Authorization', 'test');
+
+notesClient.find(new NoteFindRequest().setId(2), metadata, {}, (err, response) => {
   if (err) {
     return console.error(err);
   }
--- ts/src/server.ts
+++ ts/src/server.ts
@@ -1,6 +1,12 @@
 import { Server, ServerCredentials, ServerUnaryCall, UntypedHandleCall, sendUnaryData } from '@grpc/grpc-js';
+import { readFileSync } from 'fs';
+import { resolve } from 'path';
 import { INotesServer, NotesService } from '../proto/notes_grpc_pb';
 import { Note, NoteFindRequest, NoteFindResponse, NoteListResponse, Void } from '../proto/notes_pb';
+
+const rootCert = readFileSync(resolve(__dirname, '../../ca-cert.pem'));
+const serverCert = readFileSync(resolve(__dirname, '../../server-cert.pem'));
+const serverCertKey = readFileSync(resolve(__dirname, '../../server-key.pem'));
 
 const notes: Note.AsObject[] = [
   { id: 1, title: 'Note 1', description: 'Content 1' },
@@ -8,10 +14,11 @@ const notes: Note.AsObject[] = [
 ];
 
 class NotesServer implements INotesServer {
-
   [name: string]: UntypedHandleCall;
 
-  list(_: ServerUnaryCall<Void, NoteListResponse>, callback: sendUnaryData<NoteListResponse>) {
+  list(call: ServerUnaryCall<Void, NoteListResponse>, callback: sendUnaryData<NoteListResponse>) {
+    console.log(call.metadata);
+    
     const response = new NoteListResponse();
 
     notes.forEach(({ id, title, description }) => {
@@ -27,6 +34,8 @@ class NotesServer implements INotesServer {
   }
 
   find(call: ServerUnaryCall<NoteFindRequest, NoteFindResponse>, callback: sendUnaryData<NoteFindResponse>) {
+    console.log(call.metadata);
+    
     const id = call.request.getId();
 
     const foundNote = notes.find((note) => note.id === id);
@@ -48,9 +57,13 @@ class NotesServer implements INotesServer {
 }
 
 const server = new Server();
+
 server.addService(NotesService, new NotesServer());
 
-server.bindAsync('0.0.0.0:50052', ServerCredentials.createInsecure(), (err, port) => {
+server.bindAsync('0.0.0.0:50052', ServerCredentials.createSsl(rootCert, [{
+  private_key: serverCertKey,
+  cert_chain: serverCert,
+}]), (err, port) => {
   if (err) throw err;
   console.log(`Server is running on port ${port}`);
   server.start();
